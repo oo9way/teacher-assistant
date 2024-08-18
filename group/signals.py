7ca3a3573bbce6ec49db3425ca68027790b983ee
Muhammad Yusuf <mohammedyousef.dev@gmail.com>
2024-08-19 00:14:08 +0500

    Remove signals :

--- group/signals.py
+++ group/signals.py
@@ -6,9 +6,9 @@ from student.models import StudentTask
 from course.models import Task
 
 
-@receiver(m2m_changed, sender=Group.members.through)
-def create_student_tasks(sender, instance, action, **kwargs):
-    if action == "post_add":
-        for student in instance.members.all():
-            for task in Task.objects.all():
-                StudentTask.objects.create(student=student, task=task)
+# @receiver(m2m_changed, sender=Group.members.through)
+# def create_student_tasks(sender, instance, action, **kwargs):
+#     if action == "post_add":
+#         for student in instance.members.all():
+#             for task in Task.objects.all():
+#                 StudentTask.objects.create(student=student, task=task)
