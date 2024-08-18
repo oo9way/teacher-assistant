f0c49a76b49ddfd47dde368f57811e8ed738f3a4
Muhammad Yusuf <mohammedyousef.dev@gmail.com>
2024-08-19 00:16:12 +0500

    Remove signals :

--- group/apps.py
+++ group/apps.py
@@ -5,5 +5,5 @@ class GroupConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'group'
 
-    def ready(self):
-        from group.signals import create_student_tasks
+    # def ready(self):
+    #     from group.signals import create_student_tasks
